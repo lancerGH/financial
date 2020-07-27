4049f463317c341497a0779594a2444bee757f0a
lancerGH <xjb20130413@163.com>
2020-07-27 10:02:43 +0800

    git commit -a

--- funds.py
+++ funds.py
@@ -7,6 +7,39 @@
 3、利用CAPM对中国基金经理的投资能力进行实证研究，并给出基金的排名。
 作业提示：和题2相同，可以分为alpha收益和beta收益两部分，利用alpha收益综合beta收益来排名。
 '''
+'''
+第1题思路：
+    先假设H0中国股票市场的投资组合符合CAPM理论，显著性水平alpha=0.05；
+    收集某支基金的历史收益作为样本Y，把上证指数历史收益作为样本X
+    用最小二乘法一元线性回归成 Y=alpha + beta * X +epsilon形式的r = rf + beta * （rm-rf），得出rf和beta值
+    在CAMP理论中rf是无风险利率，现实中可能是1%之类；但是因为采取上证指数历史收益作为样本，此处的rf对应的其实是
+上证指数历史收益的期望值，也就是说0，所以老师说越接近0越符合，越偏离0越不符合。
+    用此处的rf值带入样本X中，把上证指数历史收益的均值0，方差用来计算z=（rf-mu）/(sigma/sqrt(n))，和显著性水平比较检验。
+判断是否拒绝H0。
+第2题思路：
+    以某支基金的历史收益作为样本Y，把上证指数历史收益作为样本X；
+    以最小二乘法的方式计算alpha收益和beta收益两部分；
+第3题思路：
+    按照alpha收益和beta收益的经济学解释，来对基金进行排名；
+'''
 import baostock as bs
 import numpy as np
 import pandas as pd
+
+lg=bs.login()
+
+portfolio_name="sh.000001"
+s_date ="2010-01-01"
+e_date = "2019-12-31"
+
+#导入投资组合历史数据,对象portfolio
+portfolio = bs.query_history_k_data_plus("portfolio_name","date,pctChg",                        
+    start_date= s_date, end_date= e_date, frequency="d", adjustflag="2")
+#把投资组合历史数据清理成可用的DataFrame数据结构，对象portfolio_data
+data_list=[]
+while (portfolio.error_code == '0') & portfolio.next():
+    data_list.append(portfolio.get_row_data())
+portfolio_data=pd.DataFrame(data_list,columns=portfolio.fields)
+#结果集输出到CSV文件
+portfolio_data.to_csv("portfolio_history_data.csv",index=False)
+bs.logout()
